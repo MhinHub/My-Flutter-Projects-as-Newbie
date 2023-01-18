8ba6b707efc9da3251da7acdf8214962418c3a98
MinlabHQ <iskandarm20d@student.unhas.ac.id>
2023-01-18 19:12:44 +0800

    add files

--- android/app/src/main/kotlin/com/example/fita_audio_player/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.fita.audio_player
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/models/Artist.dart
+++
@@ -1,7 +0,0 @@
-class Artist {
-  final String name;
-  final String image;
-  String get keywordSearch => name.toLowerCase().replaceAll(' ', '+');
-
-  Artist(this.name, this.image);
-}
