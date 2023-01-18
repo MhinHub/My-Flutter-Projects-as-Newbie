34ce435d77f8f4d1b108242f30d0b41267d5ed9d
MinlabHQ <iskandarm20d@student.unhas.ac.id>
2023-01-18 18:27:00 +0800

    add files

--- lib/settings.dart
+++
@@ -1,6 +0,0 @@
-/// Indicates where the progress indicators should be placed.
-enum ProgressPosition { top, bottom }
-
-/// This is used to specify the height of the progress indicator. Inline stories
-/// should use [small]
-enum IndicatorHeight { small, large }
